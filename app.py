199cd2c7326ea0884f28d02331f86fdf0475af4b
wuoc <quockhanh4114@gmail.com>
2025-06-19 22:43:15 +0700

    feat: add UI

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template_string, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from GKE by Wuoc"
+    return render_template_string(open('index.html', encoding='utf-8').read())
+
+@app.route('/api/hello')
+def api_hello():
+    return jsonify({'message': 'Hello from GKE by Wuoc'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
